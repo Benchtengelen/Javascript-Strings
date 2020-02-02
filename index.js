0be9f97f240e60509270a48399744e784541c86a
Feyyaz Yılmaz <45435495+Benchtengelen@users.noreply.github.com>
2020-02-02 17:19:10 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title></title>
+</head>
+<body>
+   <h1> Javascript </h1>
+   <script>
+
+   var ad="Wisconsin";
+   document.write(ad.toUpperCase()+"<br>");
+   document.write(ad.toLowerCase()+"<br>");
+   document.write(ad.length+"<br>");
+   document.write(ad.bold()+"<br>");
+   document.write(ad.italics()+"<br>");
+   document.write(ad.charAt(2)+"<br>");
+   document.write(ad.substring(0,5)+"<br>");
+
+</script>
+</body>
+</html>
